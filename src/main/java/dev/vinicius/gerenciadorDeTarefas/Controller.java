7d2ff85e1a72cdde13c3f001d98594181f2dfe0a
vinis <viniciusfelix1706@gmail.com>
2025-09-02 17:22:18 -0300

    Criado a Controller

---
+++ src/main/java/dev/vinicius/gerenciadorDeTarefas/Controller.java
@@ -0,0 +1,16 @@
+package dev.vinicius.gerenciadorDeTarefas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é minha primeira mesagem nessa rota";
+    }
+
+}
